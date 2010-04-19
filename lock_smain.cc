f737623bc4fa0d32fde6b28e4de293ff4f3f9615
Paarijaat Aditya <paditya@swsao1902.ds.mpi-sws.mpg.de>
2010-04-19 16:57:16 +0200

    lab1

---
+++ lock_smain.cc
@@ -0,0 +1,35 @@
+#include "rpc.h"
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lock_server.h"
+
+#include "jsl_log.h"
+
+// Main loop of lock_server
+
+int
+main(int argc, char *argv[])
+{
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
+
+  srandom(getpid());
+
+  if(argc != 2){
+    fprintf(stderr, "Usage: %s port\n", argv[0]);
+    exit(1);
+  }
+
+  //jsl_set_debug(2);
+
+#ifndef RSM
+  lock_server ls;
+  rpcs server(atoi(argv[1]));
+  server.reg(lock_protocol::stat, &ls, &lock_server::stat);
+#endif
+
+
+  while(1)
+    sleep(1000);
+}
