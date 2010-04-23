692d92397be0bc174751f0da969adaea9e923752
Byelozyorov Sergiy <rryk.ua@gmail.com>
2010-04-23 17:56:13 +0200

    Implemented server side for step one (assuming a perfect network).

--- lock_server.cc
+++ lock_server.cc
@@ -6,6 +6,51 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+// Implementation of lock_t class
+
+lock_t::lock_t()
+    : locked(false)
+{
+    pthread_cond_init(&okToLock, NULL);
+    pthread_mutex_init(&mutex, NULL);
+}
+
+void lock_t::acquire()
+{
+    pthread_mutex_lock(&mutex);
+
+    while (locked)
+        pthread_cond_wait(&okToLock, &mutex);
+
+    locked = true;
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void lock_t::release()
+{
+    pthread_mutex_lock(&mutex);
+
+    locked = false;
+
+    pthread_mutex_unlock(&mutex);
+
+    pthread_cond_signal(&okToLock);
+}
+
+bool lock_t::isLocked()
+{
+    pthread_mutex_lock(&mutex);
+
+    bool isLocked = locked;
+
+    pthread_mutex_unlock(&mutex);
+
+    return isLocked;
+}
+
+// Implementation of lock_server class
+
 lock_server::lock_server():
   nacquire (0)
 {
@@ -14,10 +59,31 @@ lock_server::lock_server():
 lock_protocol::status
 lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
 {
-  lock_protocol::status ret = lock_protocol::OK;
-  printf("stat request from clt %d\n", clt);
-  r = nacquire;
-  return ret;
+    printf("server: requested status of lock %llu\n", lid);
+
+    r = locks[lid].isLocked() ? 1 : 0;
+
+    return lock_protocol::OK;
+}
+
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &)
+{
+    printf("server: acquired lock %llu\n", lid);
+
+    locks[lid].acquire();
+
+    return lock_protocol::OK;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &)
+{
+    printf("server: released lock %llu\n", lid);
+
+    locks[lid].release();
+
+    return lock_protocol::OK;
 }
 
 
--- lock_server.h
+++ lock_server.h
@@ -9,15 +9,32 @@
 #include "lock_client.h"
 #include "rpc.h"
 
+class lock_t {
+public:
+    lock_t();
+    void acquire();
+    void release();
+    bool isLocked();
+
+private:
+    bool locked;
+    pthread_cond_t okToLock;
+    pthread_mutex_t mutex;
+};
+
 class lock_server {
 
  protected:
   int nacquire;
 
+  std::map<lock_protocol::lockid_t, lock_t> locks;
+
  public:
   lock_server();
   ~lock_server() {};
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
