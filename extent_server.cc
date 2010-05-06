29b6c6d66adde5230f2f3b547d40496b7f6dcc68
Byelozyorov Sergiy <rryk.ua@gmail.com>
2010-05-06 19:02:42 +0200

    Implemented extent server. No testing performed however.

--- extent_server.cc
+++ extent_server.cc
@@ -15,24 +15,50 @@ int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
 {
     printf("extent_server::put(id=%lld)\n", id);
 
-    return extent_protocol::IOERR;
+    // if extent exists already -- we simply overwrite it
+    // as would happen in typical file system
+
+    extent_t e;
+    e.data = buf;
+    e.attrs.mtime = e.attrs.atime = e.attrs.ctime = time(NULL);
+    e.attrs.size = buf.length();
+
+    m_dataBlocks[id] = e;
+
+    return extent_protocol::OK;
 }
 
 int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
 {
     printf("extent_server::get(id=%lld)\n", id);
 
-    return extent_protocol::IOERR;
+    // check if element exists
+    if (m_dataBlocks.find(id) != m_dataBlocks.end())
+        return extent_protocol::NOENT;
+
+    buf = m_dataBlocks[id].data;
+
+    // simulate relatime behaviour since this is default for Linux
+    // since kernel version 2.6.30
+    time_t now = time(NULL);
+    if (m_dataBlocks[id].attrs.atime < m_dataBlocks[id].attrs.ctime ||
+        m_dataBlocks[id].attrs.atime < m_dataBlocks[id].attrs.mtime ||
+        m_dataBlocks[id].attrs.atime < now - 24*60)
+        m_dataBlocks[id].attrs.atime = now;
+
+    return extent_protocol::OK;
 }
 
 int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
 {
     printf("extent_server::getattr(id=%lld)\n", id);
 
-    a.size = 0;
-    a.atime = 0;
-    a.mtime = 0;
-    a.ctime = 0;
+    // check if element exists
+    if (m_dataBlocks.find(id) != m_dataBlocks.end())
+        return extent_protocol::NOENT;
+
+    a = m_dataBlocks[id].attrs;
+
     return extent_protocol::OK;
 }
 
@@ -40,6 +66,11 @@ int extent_server::remove(extent_protocol::extentid_t id, int &)
 {
     printf("extent_server::remove(id=%lld)\n", id);
 
-    return extent_protocol::IOERR;
+    // check if element exists
+    if (m_dataBlocks.find(id) == m_dataBlocks.end())
+        return extent_protocol::NOENT;
+
+    m_dataBlocks.erase(id);
+    return extent_protocol::OK;
 }
 
