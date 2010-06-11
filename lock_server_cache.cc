9484d124129a7c27fa04175e7b1e82d3d9308ec0
Byelozyorov Sergiy <rryk.ua@gmail.com>
2010-06-11 18:06:11 +0300

    Implemented cached lock server. Compiles. No testing performed.

--- lock_server_cache.cc
+++ lock_server_cache.cc
@@ -1,10 +1,110 @@
 // the caching lock server implementation
 
+#include <queue> // causes wierd error with GCC 4.3.2 unless include is placed before header file
+                 // this quick-fix is found at http://programming.itags.org/c-c++/180035/
+
 #include "lock_server_cache.h"
+
 #include <sstream>
 #include <stdio.h>
+
 #include <unistd.h>
 #include <arpa/inet.h>
+
+// Revoke and retry request queues
+std::queue<std::pair<std::string, lock_protocol::lockid_t> > revokeRequests;
+std::queue<std::pair<std::string, lock_protocol::lockid_t> > retryRequests;
+
+// Mutexes to protect revoke and retry request queues
+pthread_mutex_t revokeMutex;
+pthread_mutex_t retryMutex;
+
+// Condition variable to allow waking up revoke and retry request handler processes
+pthread_cond_t needToRevoke;
+pthread_cond_t needToRetry;
+
+// Implementation of lock_server_cache class
+
+cache_lock_t::cache_lock_t(int lid)
+    : id(lid)
+	, lockHolder("")
+{
+	// initialize condition var
+    pthread_cond_init(&okToLock, NULL);
+    
+    // initialize mutex
+    pthread_mutex_init(&mutex, NULL);
+	
+	// initialize interested clients list
+	interestedClients.clear();
+}
+
+lock_protocol::status cache_lock_t::acquire(std::string addr)
+{
+	pthread_mutex_lock(&mutex);
+	
+	if (!lockHolder.empty())
+	{
+		// add revoke request to the queue and wake up the revoker thread
+		pthread_mutex_lock(&revokeMutex);
+		revokeRequests.push(std::make_pair(addr, id));
+		pthread_mutex_unlock(&revokeMutex);
+		pthread_cond_signal(&needToRevoke);
+		
+		// add client to the list of interested clients
+		interestedClients.push_back(addr);
+		
+		return lock_protocol::RETRY;
+	}
+	else
+	{
+		// store current lock holder
+		lockHolder = addr;
+		
+		return lock_protocol::OK;
+	}
+	
+	pthread_mutex_unlock(&mutex);
+}
+
+void cache_lock_t::release()
+{
+	pthread_mutex_lock(&mutex);
+	
+	assert(!lockHolder.empty());
+	
+	// clear lock holder
+	lockHolder = "";
+	
+	// notify all interested clients that the lock is available
+	pthread_mutex_lock(&retryMutex);
+	for (std::list<std::string>::const_iterator it = interestedClients.begin(); it != interestedClients.end(); it++)
+		revokeRequests.push(std::make_pair(*it, id)); // add repeat request to the queue
+	pthread_mutex_unlock(&retryMutex);
+	
+	// wake up the retryer thread
+	pthread_cond_signal(&needToRetry);
+	
+	// erase list of interested clients (one of them will acquire the lock and others will register again)
+	interestedClients.clear();
+	
+	pthread_mutex_unlock(&mutex);
+}
+
+bool cache_lock_t::isLocked()
+{
+	pthread_mutex_lock(&mutex);
+	
+	// copy lock status into a local var
+    bool isLocked = !lockHolder.empty();
+
+    pthread_mutex_unlock(&mutex);
+
+    // return lock status
+    return isLocked;
+}
+
+// Implementation of lock_server_cache class
 
 static void *
 revokethread(void *x)
@@ -34,23 +134,117 @@ lock_server_cache::lock_server_cache()
 void
 lock_server_cache::revoker()
 {
+	// initialize condition var
+    pthread_cond_init(&needToRevoke, NULL);
+    
+    // initialize mutex
+	pthread_mutex_init(&revokeMutex, NULL);
+	pthread_mutex_lock(&revokeMutex);
+	
+	// temp variable to store RPC result
+	int r;
 
-  // This method should be a continuous loop, that sends revoke
-  // messages to lock holders whenever another client wants the
-  // same lock
-
+	while (true)
+	{
+		// wait for new revoke requests
+		while (revokeRequests.empty())
+			pthread_cond_wait(&needToRevoke, &revokeMutex);
+			
+		// get request details
+		std::pair<std::string, lock_protocol::lockid_t> req = 
+			revokeRequests.front();
+			
+		// send revoke request
+		sockaddr_in dstsock;
+		make_sockaddr(req.first.c_str(), &dstsock);
+		rpcc cl(dstsock);
+		cl.call(rlock_protocol::revoke, req.second, r);
+		
+		// remove processed request
+		revokeRequests.pop();
+	}
+	
+	pthread_mutex_unlock(&revokeMutex);
 }
 
 
 void
 lock_server_cache::retryer()
 {
+	// initialize condition var
+    pthread_cond_init(&needToRetry, NULL);
+    
+    // initialize mutex
+	pthread_mutex_init(&retryMutex, NULL);
+	pthread_mutex_lock(&retryMutex);
+	
+	// temp variable to store RPC result
+	int r;
 
-  // This method should be a continuous loop, waiting for locks
-  // to be released and then sending retry messages to those who
-  // are waiting for it.
-
+	while (true)
+	{
+		// wait for new revoke requests
+		while (retryRequests.empty())
+			pthread_cond_wait(&needToRetry, &retryMutex);
+			
+		// get request details
+		std::pair<std::string, lock_protocol::lockid_t> req = 
+			retryRequests.front();
+			
+		// send retry request
+		sockaddr_in dstsock;
+		make_sockaddr(req.first.c_str(), &dstsock);
+		rpcc cl(dstsock);
+		cl.call(rlock_protocol::retry, req.second, r);
+		
+		// remove processed request
+		retryRequests.pop();
+	}
+	
+	pthread_mutex_unlock(&retryMutex);
 }
 
+lock_protocol::status lock_server_cache::stat(int clt, lock_protocol::lockid_t lid, int & r)
+{
+	printf("lock_server_cache::stat(%d, %llu)\n", clt, lid);
 
+    // insert new lock record on first access (for unknown-before lock id)
+	pthread_mutex_lock(&mutex);
+	if (locks.find(lid) == locks.end())
+		locks.insert(std::make_pair(lid, cache_lock_t(lid)));
+	pthread_mutex_unlock(&mutex);
+	
+    r = locks[lid].isLocked() ? 1 : 0;
 
+    return lock_protocol::OK;
+}
+
+lock_protocol::status lock_server_cache::acquire(int clt, lock_protocol::lockid_t lid, std::string rpc_addr, lock_protocol::status & r)
+{
+	printf("lock_server_cache::acquire(%d, %llu)\n", clt, lid);
+
+    // insert new lock record on first access (for unknown-before lock id)
+	pthread_mutex_lock(&mutex);
+	if (locks.find(lid) == locks.end())
+		locks.insert(std::make_pair(lid, cache_lock_t(lid)));
+	pthread_mutex_unlock(&mutex);
+	
+    r = locks[lid].acquire(rpc_addr);
+
+    return lock_protocol::OK;
+}
+
+lock_protocol::status lock_server_cache::release(int clt, lock_protocol::lockid_t lid, int &)
+{
+	printf("lock_server_cache::release(%d, %llu)\n", clt, lid);
+
+	// insert new lock record on first access (for unknown-before lock id)
+	pthread_mutex_lock(&mutex);
+	if (locks.find(lid) == locks.end())
+		locks.insert(std::make_pair(lid, cache_lock_t(lid)));
+	pthread_mutex_unlock(&mutex);
+    
+    locks[lid].release();
+
+    return lock_protocol::OK;
+}
--- lock_server_cache.h
+++ lock_server_cache.h
@@ -2,18 +2,104 @@
 #define lock_server_cache_h
 
 #include <string>
+#include <list>
+
 #include "lock_protocol.h"
 #include "rpc.h"
 #include "lock_server.h"
 
-
+/** cache_lock_t class
+ * 
+ *  This class represents a single lock on a server that can be cached 
+ *  by the client. It's purpose to provide functions that can safely 
+ *  aquire and release lock in multithreading enviroment. Additionally
+ *  it will keep the list of other clients that have previously 
+ *  requested the lock to be able to notify them whenever the lock is
+ *  successfully revoked.
+ */
+class cache_lock_t {
+public:
+	// This constructor is require to allow use of cache_lock_t as map element.
+	// Since every lock must know it's own id, then this constructor should ever 
+	// be used.
+	cache_lock_t() { assert(false); }
+	
+	/** Constructor. Initalizes internal structures and sets "unlocked" state 
+     *  for the lock.
+	 *
+	 *  @param lid Lock ID
+	 */
+    cache_lock_t(int lid);
+    
+    /** Aquires the lock. Pauses thread until lock is released.
+	 *
+	 *  @param addr Address for the RPC calls to the client
+	 *  @return Result for the acquire operation -- could be RETRY or OK
+	 */
+    lock_protocol::status acquire(std::string addr);
+    
+    /// Releases the lock. Wakes up other thread waiting to aquire this lock.
+    void release();
+    
+    /// Returns the current status of the lock.
+    bool isLocked();
+	
+private:
+	lock_protocol::lockid_t id; // current lock id
+	std::string lockHolder; // address of the current lock holder (or an empty string if noone is holding the lock)
+    pthread_cond_t okToLock; // condition on which acquire should be woken
+    pthread_mutex_t mutex; // mutex to process multi-thread access to the variables
+	std::list<std::string> interestedClients; // list of the clients that are waiting for this lock
+};
 
 class lock_server_cache {
- public:
-  lock_server_cache();
-  lock_protocol::status stat(lock_protocol::lockid_t, int &);
-  void revoker();
-  void retryer();
+public:
+	/// This is constructor for the lock server. It will start revoker
+	/// and retryer threads and set up internal variables to their initial
+	/// values.
+	lock_server_cache();
+	
+	/// This function is to be executed in a separate thread and
+	/// it will process revoke requests in a continuous loop.
+	void revoker();
+	
+	/// This function is to be executed in a separate thread and
+	/// it will process retry requests in a continuous loop.
+	void retryer();
+  
+	/** Get status of a certain lock (RPC command handler)
+	 *
+	 *  @param clt Client ID
+	 *  @param lid Lock ID
+	 *  @param r Result of the stat operation -- 1 if locked, 0 if unlocked
+	 *  @return Execution status of the RPC function. Indicates success or
+	 *          failure code.
+	 */
+	lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int & r);
+
+	/** Aquire certain lock (RPC command handler)
+	 *
+	 *  @param clt Client ID
+	 *  @param lid Lock ID
+	 *  @param rpc_addr Address for the RPC calls to the client
+	 *  @param r Result for the acquire operation -- could be RETRY or OK
+	 *  @return Execution status of the RPC function. Indicates success or
+	 *          failure code.
+	 */
+	lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, std::string rpc_addr, int & r);
+
+	/** Release certain lock (RPC command handler)
+	 *
+	 *  @param clt Client ID
+	 *  @param lid Lock ID
+	 *  @return Execution status of the RPC function. Indicates success or
+	 *          failure code.
+	 */
+	lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+	
+protected:
+    std::map<lock_protocol::lockid_t, cache_lock_t> locks; // lock map
+	pthread_mutex_t mutex; // mutex to protect map modification (such as adding new unknown-before locks)
 };
 
 #endif
